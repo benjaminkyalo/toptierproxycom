719352b7cae93d644001203b74f6fdc132cf039a
root <root@F2ov2w2VgeleSfJAZS4U.truehost-cloud.com>
2026-09-18 02:37:36 -0400

    Replace PROXYS.IO logo with updated version

--- src/components/provider-logo.tsx
+++ src/components/provider-logo.tsx
@@ -35,7 +35,7 @@ const RAW_LOGO_URL: Record<string, string> = {
   "proxy-cheap":
     "https://res.cloudinary.com/dkcqakosa/image/upload/v1777725875/proxycheap_logo_jl5ucv_pnghvr.png",
   "proxys-io":
-    "https://res.cloudinary.com/dkcqakosa/image/upload/v1789525867/logo_gsacnb.png",
+    "https://res.cloudinary.com/dkcqakosa/image/upload/v1789712333/proxy_ytzcld.png",
   "infatica":
     "https://res.cloudinary.com/dkcqakosa/image/upload/v1777725875/infatica_logo_xxzvgj_qxk5vg.jpg",
 };
